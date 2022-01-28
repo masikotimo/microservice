22a588d843dad2d6c792a143add4498159155bee
masikotimo <masikotimo@gmail.com>
2022-01-28 08:58:27 +0300

    added circle ci

--- AuthService/business_logic/management/trip_management/retrieval.py
+++
@@ -1,9 +0,0 @@
-from core.management.retrieval import Retriever
-from api.models import Trip
-
-
-class TripRetriever(Retriever):
-
-    def retrieve(self, instance_id) -> object:
-        instance = Trip.objects.get(id=instance_id)
-        return instance
